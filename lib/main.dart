6775c5ea2c18cce46ce81b091b97575b1035df93
Vedant Singh <ani.vedant@outlook.com>
2025-07-23 23:29:33 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/gradient_class.dart';
+import 'package:dice_roll/gradient_class.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(colors: [Colors.black, Color(0xFF1A1A1A)]),
+        body: GradientContainer(
+          colors: [
+            Color.fromARGB(255, 19, 0, 71),
+            Color.fromARGB(255, 19, 0, 71),
+          ],
+        ),
       ),
     ),
   );
